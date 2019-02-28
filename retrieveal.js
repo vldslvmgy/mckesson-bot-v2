708c7c62fee1b81f20ae1bc09ba45e5a7af404b4
dleung <derek.leung@realtor.com>
2019-02-28 11:16:24 -0800

    menu retrieve firsty

---
+++ retrieveal.js
@@ -0,0 +1,28 @@
+const cheerio = require('cheerio');
+const request = require('request');
+var fs = require('file-system');
+
+request({
+    method: 'GET',
+    url: 'http://www.eurestonsite.com/mckesson/Pages/Home.aspx',
+    headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X)'}
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+
+    var $ = cheerio.load(body);
+
+    $('a').each(function() {
+   var text = $(this).text();
+   var link = $(this).attr('href');
+
+   if(link && link.match(/Menus/)){
+     console.log(text + ' --> ' + link);
+     request(link, function(error, response, body){
+        fs.writeFileSync('menu.pdf',body,'binary');
+    })
+   };
+   
+ });
+
+});
